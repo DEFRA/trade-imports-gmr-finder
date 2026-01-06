ad4022127bc3590c5b0004a86179668cd0db0820
Phil Morgan <phil@psmorgan.co.uk>
2026-01-06 14:45:01 +0000

    PHAIN-237: Update positive logging and add tests
    When things go well we want to log it.

--- tests/GmrFinder.Tests/Producers/MatchedGmrsProducerTests.cs
+++ tests/GmrFinder.Tests/Producers/MatchedGmrsProducerTests.cs
@@ -3,6 +3,7 @@ using Amazon.SimpleNotificationService;
 using Amazon.SimpleNotificationService.Model;
 using AutoFixture;
 using Defra.TradeImportsGmrFinder.Domain.Events;
+using Defra.TradeImportsGmrFinder.GvmsClient.Contract;
 using GmrFinder.Configuration;
 using GmrFinder.Producers;
 using Microsoft.Extensions.Logging;
@@ -17,12 +18,12 @@ public class MatchedGmrsProducerTests
         new MatchedGmrsProducerOptions { TopicArn = "topic_name" }
     );
     private readonly Mock<IAmazonSimpleNotificationService> _mockSns = new();
-    private readonly ILogger<MatchedGmrsProducer> _mockLogger = Mock.Of<ILogger<MatchedGmrsProducer>>();
+    private readonly Mock<ILogger<MatchedGmrsProducer>> _mockLogger = new();
 
     [Fact]
     public async Task PublishMatchedGmrs_WithNoMatchedGmrsProvided_DoesNothing()
     {
-        var producer = new MatchedGmrsProducer(_mockLogger, _mockSns.Object, _options);
+        var producer = new MatchedGmrsProducer(_mockLogger.Object, _mockSns.Object, _options);
         await producer.PublishMatchedGmrs([], CancellationToken.None);
 
         _mockSns.Verify(
@@ -37,7 +38,7 @@ public class MatchedGmrsProducerTests
         var fixture = new Fixture();
         var matchedGmrs = fixture.CreateMany<MatchedGmr>(35).ToList();
 
-        var producer = new MatchedGmrsProducer(_mockLogger, _mockSns.Object, _options);
+        var producer = new MatchedGmrsProducer(_mockLogger.Object, _mockSns.Object, _options);
 
         List<PublishBatchRequest> publishBatchRequests = [];
         _mockSns
@@ -57,5 +58,59 @@ public class MatchedGmrsProducerTests
 
         var expectedId = matchedGmrs[0].Mrn + "-" + matchedGmrs[0].Gmr.GmrId;
         entry.Id.Should().BeEquivalentTo(expectedId);
+    }
+
+    [Fact]
+    public async Task PublishMatchedGmrs_WithMatchedGmrs_LogsPublishingMessage()
+    {
+        var matchedGmrs = new List<MatchedGmr>
+        {
+            new()
+            {
+                Mrn = "MRN1",
+                Gmr = new Gmr
+                {
+                    GmrId = "GMR1",
+                    HaulierEori = "GB111",
+                    State = "COMPLETED",
+                    UpdatedDateTime = DateTime.UtcNow.ToString("O"),
+                    Direction = "Inbound",
+                },
+            },
+            new()
+            {
+                Mrn = "MRN2",
+                Gmr = new Gmr
+                {
+                    GmrId = "GMR2",
+                    HaulierEori = "GB222",
+                    State = "COMPLETED",
+                    UpdatedDateTime = DateTime.UtcNow.ToString("O"),
+                    Direction = "Inbound",
+                },
+            },
+        };
+
+        var producer = new MatchedGmrsProducer(_mockLogger.Object, _mockSns.Object, _options);
+
+        _mockSns
+            .Setup(x => x.PublishBatchAsync(It.IsAny<PublishBatchRequest>(), It.IsAny<CancellationToken>()))
+            .ReturnsAsync(new PublishBatchResponse());
+
+        await producer.PublishMatchedGmrs(matchedGmrs, CancellationToken.None);
+
+        var expectedPairs = string.Join(",", matchedGmrs.Select(m => $"{m.Mrn}:{m.Gmr.GmrId}"));
+        var expectedMessage = $"Publishing matched MRN:GMRs: {expectedPairs}";
+        _mockLogger.Verify(
+            logger =>
+                logger.Log(
+                    LogLevel.Information,
+                    It.IsAny<EventId>(),
+                    It.Is<It.IsAnyType>((state, _) => state.ToString()!.Equals(expectedMessage)),
+                    It.IsAny<Exception>(),
+                    It.IsAny<Func<It.IsAnyType, Exception?, string>>()
+                ),
+            Times.Once
+        );
     }
 }
