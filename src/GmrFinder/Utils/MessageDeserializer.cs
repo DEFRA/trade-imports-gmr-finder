65081ceef3de742cfe6cdacdfd4cd5c30eee188c
Phil Morgan <phil@psmorgan.co.uk>
2026-02-25 11:27:37 +0000

    PHAIN-254: Bump the Data Api dependencies and use the web deserialiser (#62)
    This bumps the Data Api dependencies so we have the latest versions and updates to use the web deserialiser, ensuring we deserialise correctly.

--- src/GmrFinder/Utils/MessageDeserializer.cs
+++ src/GmrFinder/Utils/MessageDeserializer.cs
@@ -6,6 +6,8 @@ namespace GmrFinder.Utils;
 
 public static class MessageDeserializer
 {
+    private static readonly JsonSerializerOptions s_defaultSerializerOptions = new(JsonSerializerDefaults.Web);
+
     public static T? Deserialize<T>(string message, string? contentEncoding)
     {
         if (contentEncoding != null && contentEncoding != "gzip, base64")
@@ -16,12 +18,12 @@ public static class MessageDeserializer
         }
 
         if (contentEncoding == null)
-            return JsonSerializer.Deserialize<T>(message);
+            return JsonSerializer.Deserialize<T>(message, s_defaultSerializerOptions);
 
         var compressedBytes = Convert.FromBase64String(message);
         using var compressedStream = new MemoryStream(compressedBytes);
         using var gzipStream = new GZipStream(compressedStream, CompressionMode.Decompress);
 
-        return JsonSerializer.Deserialize<T>(gzipStream);
+        return JsonSerializer.Deserialize<T>(gzipStream, s_defaultSerializerOptions);
     }
 }
